4b5bfe51a795278d9f315fea23a5618de3967f9c
jeremyconsultant <jeremy.pereirapires@gmail.com>
2026-05-16 17:29:44 +0200

    update project

--- api/retraite-candidature.js
+++ api/retraite-candidature.js
@@ -52,7 +52,7 @@ export default async function handler(req, res) {
     const fullName = `${prenom} ${nom}`.trim();
 
     await Promise.all([
-      fetch('https://hook.eu2.make.com/yt61c14ht88b8m6bofpwlcs8ibxkjjat', {
+      fetch('https://hook.eu2.make.com/dm0ysdoe77zk67ad8c52ijush6dmh96g', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body)
